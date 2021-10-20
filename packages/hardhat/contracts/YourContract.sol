405a2a4440a49344f7f1b79db8229a593af54c1e
wheeei <7512280+alexanderanter@users.noreply.github.com>
2021-10-20 16:50:14 +0100

    adding in deposit and withdraw

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -11,12 +11,21 @@ contract YourContract is Ownable {
   string public purpose = "Building Unstoppable Appps!!x";
 
   constructor() {
+    //set the owner to a different address than the deploy wallet
    transferOwnership(0xfbC798F5eFB753f4fa81BE2e56cC68E23d6cD007);
   }
 
-  function setPurpose(string memory newPurpose) public onlyOwner {
+  function setPurpose(string memory newPurpose) public payable {
+    //make sure user pays 0.001 ether in order to change purpose
+    require(msg.value == 0.001 ether, "NOT ENOUGH");
       purpose = newPurpose;
       console.log(msg.sender,"set purpose to",purpose);
       //emit SetPurpose(msg.sender, purpose);
+  }
+
+  function withdraw() public onlyOwner {
+      (bool success, bytes memory data) = msg.sender.call{value: address(this).balance}("");
+      require( success, "FAILED");
+    
   }
 }
